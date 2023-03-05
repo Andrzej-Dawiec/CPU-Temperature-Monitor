2bee8ab031a6ae84b7774868bbb3ba5d83164661
Andrzej-Dawiec <114450529+Andrzej-Dawiec@users.noreply.github.com>
2023-03-05 00:31:12 -0800

    Add files via upload

---
+++ temp.py
@@ -0,0 +1,7 @@
+import time
+
+while True:
+    with open('/sys/class/thermal/thermal_zone0/temp', 'r') as f:
+        temp = int(f.read()) / 1000.0
+    print(f'temp = {temp:.1f} °C')
+    time.sleep(5)
